3a81bf4d366c36c521da10a2838d88e3ec05ae8b
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2025-04-14 10:54:56 +0530

    import express and check server is running or not

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("Hello Node Js");
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
